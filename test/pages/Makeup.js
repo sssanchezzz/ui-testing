170c69810e172f2d4548c0e6893f40167c84c63c
Sasha <61244342+sssanchezzz@users.noreply.github.com>
2021-12-04 18:51:36 +0200

    ui testing

---
+++ test/pages/Makeup.js
@@ -0,0 +1,170 @@
+// import { launch } from 'puppeteer';
+const puppeteer = require('puppeteer');
+
+class Makeup {
+    async openMakeup(url) {
+        this.browser = await puppeteer.launch({
+            headless: false,
+        });
+        const page = await this.browser.newPage();
+
+        await page.setViewport({ width: 1440, height: 900 });
+        await page.goto(url);
+        // await page.waitForTimeout(2000);
+
+        return page;
+    }
+
+    async loginPage(makeupPage) {
+        async function login(email, password) {
+            // await makeupPage.goto('https://makeup.com.ua/');
+            await makeupPage.waitForSelector(
+                'body > div.site-wrap > div.main-wrap > header > div.header-top > div > div:nth-child(3) > div'
+            );
+            await makeupPage.click(
+                'body > div.site-wrap > div.main-wrap > header > div.header-top > div > div:nth-child(3) > div'
+            );
+            await makeupPage.waitForTimeout(300);
+            await makeupPage.waitForSelector(
+                '#form-auth > div > div.form-inner-wrap > div:nth-child(2) > div > input'
+            );
+            await makeupPage.type(
+                '#form-auth > div > div.form-inner-wrap > div:nth-child(2) > div > input',
+                email,
+                { delay: 200 }
+            );
+            await makeupPage.waitForSelector(
+                '#form-auth > div > div.form-inner-wrap > div:nth-child(3) > div > input'
+            );
+            await makeupPage.type(
+                '#form-auth > div > div.form-inner-wrap > div:nth-child(3) > div > input',
+                password,
+                { delay: 200 }
+            );
+            await makeupPage.click(
+                '#form-auth > div > div.form-inner-wrap > div:nth-child(4) > button'
+            );
+            await makeupPage.waitForTimeout(1000);
+            return makeupPage;
+        }
+        // async function loginToValidate(email, password) {
+        //     await login(email, password);
+        // }
+        async function mainPageLoggedIn(email, password) {
+            const loginP = login(email, password);
+            // return await this.mainPage(loginP);
+        }
+
+        return {
+            login: login,
+            // loginToValidate: loginToValidate,
+            mainPageLoggedIn: mainPageLoggedIn,
+        };
+    }
+    async mainPage(makeupPage) {
+        await makeupPage.click(
+            'body > div.site-wrap > div.main-wrap > header > div.header-middle > div > a'
+        );
+        return makeupPage;
+    }
+    async addToCart(makeupPage) {
+        await makeupPage.waitForSelector(
+            'body > div.site-wrap > div.main-wrap > div > div > div.catalog > div.catalog-content > div > div.catalog-products > ul > li:nth-child(1) > div.simple-slider-list__link'
+        );
+        await makeupPage.click(
+            'body > div.site-wrap > div.main-wrap > div > div > div.catalog > div.catalog-content > div > div.catalog-products > ul > li:nth-child(1) > div.simple-slider-list__link'
+        );
+        await console.log('wait for buy button');
+        await makeupPage.waitForSelector(
+            'body > div.site-wrap > div.main-wrap > div > div > div:nth-child(2) > div.product-item > div > div.product-item__buy > div.product-item__button > div'
+        );
+        await console.log('click at buy button');
+
+        await makeupPage.click(
+            'body > div.site-wrap > div.main-wrap > div > div > div:nth-child(2) > div.product-item > div > div.product-item__buy > div.product-item__button > div'
+        );
+        await console.log('wait for popup');
+
+        await makeupPage.waitForSelector(
+            'body > div.popup.cart.ng-animate.ng-hide-animate'
+        );
+        return makeupPage;
+    }
+
+    async search(makeupPage) {
+        async function searchByName(name) {
+            // await notinoPage.waitForSelector(
+            //     '#pageHeader > div._1LInFm5fZYpJXSYdiJTWxA > div._1NsUphVxu401-D-6FsTwkL.oFmVYvVlFW0TQ3LGMLmdr > div._1nzKQJY2XIaDq4UzwTO0Mj._2tH8mGWs99eOKMPn1a9z29 > input'
+            // );
+            await makeupPage.click('#search-input');
+            await makeupPage.type('#search-input', name);
+            await makeupPage.keyboard.press('Enter');
+            //or
+            await makeupPage.waitForSelector(
+                'body > div.site-wrap > div.main-wrap > div > div > div.search-results.info-text'
+            );
+
+            return makeupPage;
+        }
+        async function orderByPrice() {
+            await makeupPage.waitForSelector('#filter-sort');
+
+            // await page.waitForTimeout(2000);
+            await makeupPage.waitForSelector('#col-content');
+            await makeupPage.click('#filter-sort');
+            await makeupPage.click('#sort-3');
+            return makeupPage;
+        }
+        async function addToFavourites() {
+            await makeupPage.waitForTimeout(1000);
+            // await makeupPage.hover(
+            //     'body > div.site-wrap > div.main-wrap > div > div > div.catalog > div.catalog-content > div > div.catalog-products > ul > li:nth-child(2) > div.simple-slider-list__link'
+            // );
+            await makeupPage.waitForSelector(
+                'body > div.site-wrap > div.main-wrap > div > div > div.catalog > div.catalog-content > div > div.catalog-products > ul > li:nth-child(1) > div.simple-slider-list__link'
+            );
+            await makeupPage.click(
+                'body > div.site-wrap > div.main-wrap > div > div > div.catalog > div.catalog-content > div > div.catalog-products > ul > li:nth-child(1) > div.simple-slider-list__link'
+            );
+            await makeupPage.waitForSelector(
+                'body > div.site-wrap > div.main-wrap > div > div > div:nth-child(2) > div.product-item > div > div.product-item__buy > div.product-item__button > div.product__to-favourite'
+            );
+            await makeupPage.click(
+                'body > div.site-wrap > div.main-wrap > div > div > div:nth-child(2) > div.product-item > div > div.product-item__buy > div.product-item__button > div.product__to-favourite'
+            );
+            await makeupPage.waitForSelector('#popup__window');
+            return makeupPage;
+        }
+
+        return {
+            searchByName: searchByName,
+            orderByPrice: orderByPrice,
+            addToFavourites: addToFavourites,
+        };
+    } // click on search и ввести название
+    async showSales(makeupPage) {
+        await makeupPage.waitForSelector(
+            'div._3_jd-z3Nu-tJuU03tddE2B._2T3S1Jgb_rsSUnXe5qSkEn > div._1jCjQvInPzXO06694Sapn- > div:nth-child(3) > a > div'
+        );
+        await makeupPage.click(
+            'div._3_jd-z3Nu-tJuU03tddE2B._2T3S1Jgb_rsSUnXe5qSkEn > div._1jCjQvInPzXO06694Sapn- > div:nth-child(3) > a > div'
+        );
+
+        return makeupPage;
+    } //click on акции
+
+    async endTest() {
+        this.browser.close();
+    }
+    // getPic();
+    // openNotino();
+    // login(openNotino());
+    // let page = openNotino();
+    // let logg = loginPage(page, email, password);
+
+    // mainPage(page);
+    // loginPage.mainPageLoggedIn(email, password)
+}
+module.exports.Makeup = Makeup;
+// const _Notino = Notino;
+// export { _Notino as Notino };
---
+++ test/specs/test.js
@@ -0,0 +1,149 @@
+const reporter = require('@wdio/allure-reporter').default;
+const { email, password } = require('../../credentials');
+const { Makeup } = require('../pages/Makeup');
+const { expect } = require('chai');
+// const { done: Done } = require('mocha');
+
+describe('Testing login', function () {
+    let page;
+    let makeup = new Makeup();
+    this.timeout(12000);
+    before(async () => {
+        page = await makeup.openMakeup('https://makeup.com.ua/');
+    });
+    it('Typed email', async function () {
+        // console.log(login);
+        reporter.addStep('Testing login');
+        reporter.addDescription('testing login to makeup');
+        reporter.startStep('entering email and password');
+
+        const loginPage = await makeup.loginPage(page);
+        const login = await loginPage.login(email, password);
+        reporter.endStep();
+
+        await login.waitForTimeout(300);
+        const cab = await login.evaluate(() => {
+            return document.querySelector(
+                'body > div.site-wrap > div.main-wrap > header > div.header-top > div > div:nth-child(3) > a'
+            ).innerText;
+        });
+        reporter.startStep('check if logged in');
+
+        console.log(cab);
+        expect(cab === 'Кабинет');
+        reporter.endStep();
+    });
+    this.afterAll(async () => {
+        await makeup.endTest();
+    });
+});
+
+describe('Testing Search', async function () {
+    let page;
+    let makeup = new Makeup();
+    const searchString = 'makeup revolution';
+    this.timeout(12000);
+    before(async () => {
+        page = await makeup.openMakeup('https://makeup.com.ua/');
+    });
+    it('Entering Search Keywords', async function () {
+        // console.log(login);
+        reporter.addStep('Testing search');
+        reporter.addDescription('Testing search on makeup.com');
+        reporter.startStep('Entering keywords');
+
+        const search = await makeup.search(page);
+        const { searchByName } = search;
+        const searchResults = await searchByName(searchString);
+        await searchResults.waitForTimeout(300);
+        const cab = await searchResults.evaluate(() => {
+            return document.querySelector(
+                'body > div.site-wrap > div.main-wrap > div > div > div.search-results.info-text'
+            ).innerText;
+        });
+        // console.log('hahahakfld jfks');
+        console.log(cab);
+
+        expect(cab.includes(searchString));
+        reporter.endStep();
+    });
+    this.afterAll(async () => {
+        await makeup.endTest();
+    });
+});
+
+describe('Testing Adding To Cart', async function () {
+    let page;
+    let makeup = new Makeup();
+    const searchString = 'makeup revolution';
+    this.timeout(12000);
+    before(async () => {
+        page = await makeup.openMakeup('https://makeup.com.ua/');
+    });
+    it('Searching for a product and adding it to the cart', async function () {
+        // console.log(login);
+        reporter.addStep('Testing adding to cart');
+        reporter.addDescription('Testing adding searched item to cart');
+        reporter.startStep('search keywords');
+
+        const search = await makeup.search(page);
+        const { searchByName } = search;
+        const searchResults = await searchByName(searchString);
+        // await searchResults.waitForTimeout(300);
+        const addCart = await makeup.addToCart(searchResults);
+
+        // const fav = await addToFavourites();
+
+        const cab = await addCart.evaluate(() => {
+            return document.querySelector(
+                'body > div.popup.cart.ng-animate.ng-hide-animate > div > div.popup-content > div.page-header'
+            ).innerHTML;
+        });
+
+        console.log(cab);
+        expect(cab.includes('Корзина'));
+        reporter.endStep();
+    });
+    this.afterAll(async () => {
+        await makeup.endTest();
+    });
+});
+
+describe('Testing Adding To Favourites', async function () {
+    let page;
+    let makeup = new Makeup();
+    const searchString = 'makeup revolution';
+    this.timeout(20000);
+    before(async () => {
+        page = await makeup.openMakeup('https://makeup.com.ua/');
+    });
+    it('Searching for a product and adding it to favourites', async function () {
+        // console.log(login);
+        reporter.addStep('Testing adding to favourites');
+        reporter.addDescription('testing adding searched item to favourites');
+        reporter.startStep('entering keywords');
+        const { login } = await makeup.loginPage(page);
+        const loggedPage = await login(email, password);
+
+        const search = await makeup.search(loggedPage);
+        const { searchByName } = search;
+        const searchResults = await searchByName(searchString);
+        // await searchResults.waitForTimeout(300);
+        const { addToFavourites } = await makeup.search(searchResults);
+        const fav = await addToFavourites();
+
+        // const fav = await addToFavourites();
+        const cab = await fav.evaluate(() => {
+            return document.querySelector(
+                '#popup__window > div.popup-content > div > div.confirm-wish-list-changes.specify-message-block'
+            ).textContent;
+        });
+
+        console.log(cab);
+        expect(cab.includes('получать'));
+        reporter.endStep();
+    });
+    this.afterAll(async () => {
+        await makeup.endTest();
+    });
+});
